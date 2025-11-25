4b56a5218de83e399c445e49787fc8875038ac79
justhuuthang <95196568+justhuuthang@users.noreply.github.com>
2025-11-26 03:32:25 +0700

    Add backend

---
+++ src/main/java/com/hucework/quizzonline/QuizzOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.hucework.quizzonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizzOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizzOnlineApplication.class, args);
+	}
+
+}
